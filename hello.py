147f78d1e764f65429ff06a3dff4c0a96693b6a7
Apex324-star <samuelayalew3150@gmail.com>
2026-06-30 10:13:41 +0300

    first py code

---
+++ hello.py
@@ -0,0 +1,11 @@
+def factorial(n):
+   if n==0 or n ==1:
+       return 1
+    return n*factorial(n-1)
+try:    
+  nu=int(input("Enter number to be factorized"))
+except:
+  print("data type should be intiger")
+else:   
+  print(f"the factorial of {nu} is {factorial(nu)
+          
